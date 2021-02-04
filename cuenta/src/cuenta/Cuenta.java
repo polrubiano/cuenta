2df37e7bd6014ae517d1ff53561129c2de665eaf
Diego <pol_rubiano@yahoo.es>
2021-02-04 17:00:30 +0100

    añadidos métodos a Cuenta

--- cuenta/src/cuenta/Cuenta.java
+++ cuenta/src/cuenta/Cuenta.java
@@ -1,5 +1,50 @@
 package cuenta;
 
 public class Cuenta {
+	private String titular;
+	private double cantidad;
+	
+	Cuenta(){
+		this.titular = null;
+		this.cantidad = 0;
+	}
+	
+	Cuenta(String titular, double cantidad){
+		this.titular = titular;
+		this.cantidad = cantidad;
+	}
 
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public String toString() {
+		return "titular: " + this.titular + "\ncantidad: " + cantidad;
+	}
+	
+	public void ingresar (double cantidad) {
+		if (cantidad > 0) {
+			this.cantidad += cantidad;
+		}
+	}
+	
+	public void retirar (double cantidad) {
+		if ((this.cantidad - cantidad) <= 0) {
+			this.cantidad = 0;
+		}else {
+			this.cantidad -= cantidad;
+		}
+	}
 }
